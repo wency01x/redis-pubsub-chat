cbff5e5bc385237c3aa3a6ee7b51be10cc0c1c96
wency01x <wencywego21@gmail.com>
2026-03-20 12:30:18 +0800

    add core python scripts

---
+++ src/config.py
@@ -0,0 +1,31 @@
+import os 
+import redis 
+from dotenv import load_dotenv
+
+#load secret variabes from env file
+load_dotenv()
+
+def get_redis_connection():
+    try:
+        host = os.getenv("REDIS_HOST", "localhost")
+        port = os.getenv("REDIS_PORT", 6379)
+        password = os.getenv("REDIS_PASSWORD", None)
+
+        client = redis.Redis(
+            host=host,
+            port=port,
+            password=password,
+            decode_responses=True
+        )
+
+        if client.ping():
+            return client
+    
+    except Exception as e:
+        print(f"Error connecting to Redis: {e}")
+        return None
+
+if __name__ == "__main__":
+    #a quick test to run later
+    if get_redis_connection():
+        print("Successfully connected to Redis!")
